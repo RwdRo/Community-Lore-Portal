f4a6b5d267a658160772899b926f739862ac2bfd
Rohan van der Sandt <81447858+RwdRo@users.noreply.github.com>
2026-03-26 15:57:05 +0200

    Delete src directory

--- src/simpleUI.js
+++
@@ -1,9 +0,0 @@
-import { AbstractUserInterface } from '@wharfkit/session'
-
-export class SimpleUI extends AbstractUserInterface {
-    onLogin() {}
-    onLoginResult(result) {}
-    onError(error) {
-        console.error("Wharfkit UI error", error);
-    }
-}
